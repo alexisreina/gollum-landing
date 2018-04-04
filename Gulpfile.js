1b2ea244d537096ba73c8571ae396082f35279b8
Alexis Reina <alexis@coldbridgestudio.com>
2018-04-04 16:45:14 +0200

    copy images to dist

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,31 +1,39 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync')
-const inlineSource = require('gulp-inline-source');
+const inlineSource = require('gulp-inline-source')
 const reload = browserSync.reload
 const stream = browserSync.stream
 
-gulp.task('default', ['webfonts', 'inlinesource'], () => {
+gulp.task('default', ['webfonts', 'images', 'inlinesource'], () => {
 
   browserSync.init({
-    server: './dist'
+    server: './dist',
   })
 
   gulp.watch('./src/css/*.css', ['inlinesource'])
   gulp.watch('./src/*.html', ['inlinesource'])
-});
+})
 
 gulp.task('inlinesource', () => {
   const options = {
-    compress: false
-  };
+    compress: false,
+  }
 
-  return gulp.src('./src/*.html')
+  return gulp
+    .src('./src/*.html')
     .pipe(inlineSource(options))
     .pipe(gulp.dest('./dist'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 })
 
 gulp.task('webfonts', () => {
-  return gulp.src('./src/fonts/*.{eot,ttf,svg,woff,woff2}')
+  return gulp
+    .src('./src/fonts/*.{eot,ttf,svg,woff,woff2}')
     .pipe(gulp.dest('./dist/fonts'))
+})
+
+gulp.task('images', () => {
+  return gulp
+    .src('./src/img/*.{gif,jpg,jpeg,png,svg}')
+    .pipe(gulp.dest('./dist/img'))
 })
